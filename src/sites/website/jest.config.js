d408009f38184baaf3114c71de756ddec2d0efa4
Peter Parker <pavanteja.bhatta@gmail.com>
2020-09-13 19:27:03 +1000

    Architecture design setup

---
+++ src/sites/website/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'website',
+  preset: '../../../jest.config.js',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
