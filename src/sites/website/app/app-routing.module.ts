67983aeb3254be0930a29fc0dc1551bb0d7c6c97
Peter Parker <pavanteja.bhatta@gmail.com>
2020-10-04 20:38:41 +1000

    feat(website): Creating coming soon component

--- src/sites/website/app/app-routing.module.ts
+++ src/sites/website/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComingSoonComponent } from './containers';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '', redirectTo: 'coming-soon', pathMatch: 'full'
+	}, {
+		path: 'coming-soon',
+		component: ComingSoonComponent,
+	}, {
+		path: '**',
+		redirectTo: 'coming-soon'
+	}
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- src/sites/website/app/app.module.ts
+++ src/sites/website/app/app.module.ts
@@ -2,19 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ComingSoonComponent, RootComponent } from './containers';
 import { CoreModule } from "@projekt/core";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-	AppRoutingModule,
-	CoreModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		RootComponent,
+		ComingSoonComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		CoreModule
+	],
+	providers: [],
+	bootstrap: [RootComponent]
 })
 export class AppModule { }
