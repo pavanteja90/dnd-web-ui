5c572ed55a2c78f9dceb7328da5284224b2fc225
Peter Parker <pavanteja.bhatta@gmail.com>
2023-04-09 19:09:00 +1000

    Angular 15 upgrade

--- src/sites/website/app/app-routing.module.ts
+++ src/sites/website/app/app-routing.module.ts
@@ -1,121 +1,144 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MetaGuard } from '@projekt/core';
-import { AboutUsComponent, CareersComponent, ContactUsComponent, FemaleMenuComponent, HomeComponent, MaleMenuComponent, OfferedServicesComponent, ProductsComponent, PromosComponent, StoreMenuComponent } from './components';
+import {
+	AboutUsComponent,
+	CareersComponent,
+	ContactUsComponent,
+	FemaleMenuComponent,
+	HomeComponent,
+	MaleMenuComponent,
+	OfferedServicesComponent,
+	ProductsComponent,
+	PromosComponent,
+	StoreMenuComponent,
+} from './components';
 import { ComingSoonComponent } from './containers';
 
 const routes: Routes = [
 	{
 		path: '',
 		pathMatch: 'full',
-		redirectTo: 'home'
-	}, {
+		redirectTo: 'home',
+	},
+	{
 		path: 'coming-soon',
 		component: ComingSoonComponent,
 		canActivate: [MetaGuard],
 		data: {
 			meta: {
-				title: "Coming Soon"
-			}
-		}
-	}, {
+				title: 'Coming Soon',
+			},
+		},
+	},
+	{
 		path: 'about-us',
 		component: AboutUsComponent,
 		canActivate: [MetaGuard],
 		data: {
 			meta: {
-				title: "About us"
-			}
-		}
-	}, {
+				title: 'About us',
+			},
+		},
+	},
+	{
 		path: 'careers',
 		component: CareersComponent,
 		canActivate: [MetaGuard],
 		data: {
 			meta: {
-				title: "Careers"
-			}
-		}
-	}, {
+				title: 'Careers',
+			},
+		},
+	},
+	{
 		path: 'contact-us',
 		component: ContactUsComponent,
 		canActivate: [MetaGuard],
 		data: {
 			meta: {
-				title: "Contact us"
-			}
-		}
-	}, {
+				title: 'Contact us',
+			},
+		},
+	},
+	{
 		path: 'services',
 		component: OfferedServicesComponent,
 		canActivate: [MetaGuard],
 		data: {
 			meta: {
-				title: "Services"
-			}
-		}
-	}, {
+				title: 'Services',
+			},
+		},
+	},
+	{
 		path: 'services/male',
 		component: MaleMenuComponent,
 		canActivate: [MetaGuard],
 		data: {
 			meta: {
-				title: "Male Services"
-			}
-		}
-	}, {
+				title: 'Male Services',
+			},
+		},
+	},
+	{
 		path: 'services/female',
 		component: FemaleMenuComponent,
 		canActivate: [MetaGuard],
 		data: {
 			meta: {
-				title: "Female Services"
-			}
-		}
-	}, {
+				title: 'Female Services',
+			},
+		},
+	},
+	{
 		path: 'products',
 		component: ProductsComponent,
 		canActivate: [MetaGuard],
 		data: {
 			meta: {
-				title: "Products"
-			}
-		}
-	}, {
+				title: 'Products',
+			},
+		},
+	},
+	{
 		path: 'promos',
 		component: PromosComponent,
 		canActivate: [MetaGuard],
 		data: {
 			meta: {
-				title: "Promotions"
-			}
-		}
-	}, {
+				title: 'Promotions',
+			},
+		},
+	},
+	{
 		path: 'menu',
 		component: StoreMenuComponent,
 		canActivate: [MetaGuard],
 		data: {
 			meta: {
-				title: "Menu"
-			}
-		}
-	}, {
+				title: 'Menu',
+			},
+		},
+	},
+	{
 		path: 'home',
 		component: HomeComponent,
 		canActivate: [MetaGuard],
 		data: {
 			meta: {
-				title: "Home"
-			}
-		}
-	}, {
+				title: 'Home',
+			},
+		},
+	},
+	{
 		path: '**',
-		redirectTo: 'home'
-	}
+		redirectTo: 'home',
+	},
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-	exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes, {})],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
