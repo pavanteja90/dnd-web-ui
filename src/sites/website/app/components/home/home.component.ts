5fd83d77dff6be51f86edc1d808d49692534c046
Peter Parker <pavanteja.bhatta@gmail.com>
2023-04-11 17:09:53 +1000

    Webp images for salon menu

--- src/sites/website/app/components/home/home.component.ts
+++ src/sites/website/app/components/home/home.component.ts
@@ -8,18 +8,18 @@ import { ConfigService } from '@projekt/core';
 export class HomeComponent {
 	bitmoji: string = `assets/img/bitmoji/` + this.configService.config.custom.home.bitmoji;
 	public images = [
-		{ path: "assets/img/carousel/1.jpg", alt: "Sainikpuri salon image 1" },
-		{ path: "assets/img/carousel/2.jpg", alt: "Sainikpuri salon image 2" },
-		{ path: "assets/img/carousel/3.jpg", alt: "Sainikpuri salon image 3" },
-		{ path: "assets/img/carousel/4.jpg", alt: "Sainikpuri salon image 4" },
-		{ path: "assets/img/carousel/5.jpg", alt: "Sainikpuri salon image 5" },
-		{ path: "assets/img/carousel/6.jpg", alt: "Sainikpuri salon image 6" },
-		{ path: "assets/img/carousel/7.jpg", alt: "Sainikpuri salon image 7" },
-		{ path: "assets/img/carousel/8.jpg", alt: "Sainikpuri salon image 8" }
+		{ path: "assets/img/carousel/1.webp", alt: "Sainikpuri salon image 1" },
+		{ path: "assets/img/carousel/2.webp", alt: "Sainikpuri salon image 2" },
+		{ path: "assets/img/carousel/3.webp", alt: "Sainikpuri salon image 3" },
+		{ path: "assets/img/carousel/4.webp", alt: "Sainikpuri salon image 4" },
+		{ path: "assets/img/carousel/5.webp", alt: "Sainikpuri salon image 5" },
+		{ path: "assets/img/carousel/6.webp", alt: "Sainikpuri salon image 6" },
+		{ path: "assets/img/carousel/7.webp", alt: "Sainikpuri salon image 7" },
+		{ path: "assets/img/carousel/8.webp", alt: "Sainikpuri salon image 8" }
 	];
 
 	public promotions = [
-		{ path: "assets/img/promotions/current/1.gif", alt: "30% off promotion" }
+		{ path: "assets/img/promotions/current/1.gif", alt: "20% off promotion" }
 	];
 
 	constructor(private configService: ConfigService) { }
