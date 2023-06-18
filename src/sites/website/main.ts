23a281d972f48b4277d921d9a1e8d04e25697094
Peter Parker <pavanteja.bhatta@gmail.com>
2023-06-18 14:21:03 +1000

    Adding vercel analytics

--- src/sites/website/main.ts
+++ src/sites/website/main.ts
@@ -3,10 +3,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { inject } from '@vercel/analytics';
 
 if (environment.production) {
-  enableProdMode();
+	inject();
+	enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+	.bootstrapModule(AppModule)
+	.catch((err) => console.error(err));
