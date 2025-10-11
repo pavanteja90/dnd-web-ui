263cc080de2c6d7acb4855cfdd6d7beb85436eba
Pavan Bhatta <pavanteja.bhatta@gmail.com>
2025-10-11 10:12:49 +1000

    Adding new sainikpuri images

--- src/sites/website/app/components/home/home.component.ts
+++ src/sites/website/app/components/home/home.component.ts
@@ -4,27 +4,85 @@ import { Component } from '@angular/core';
 	templateUrl: 'home.component.html',
 })
 export class HomeComponent {
-	public images = [
-		{ path: 'assets/img/carousel/1.webp', alt: 'Sainikpuri salon image 1' },
-		{ path: 'assets/img/carousel/2.webp', alt: 'Sainikpuri salon image 2' },
-		{ path: 'assets/img/carousel/3.webp', alt: 'Sainikpuri salon image 3' },
-		{ path: 'assets/img/carousel/4.webp', alt: 'Sainikpuri salon image 4' },
-		{ path: 'assets/img/carousel/5.webp', alt: 'Sainikpuri salon image 5' },
-		{ path: 'assets/img/carousel/6.webp', alt: 'Sainikpuri salon image 6' },
-		{ path: 'assets/img/carousel/7.webp', alt: 'Sainikpuri salon image 7' },
-		{ path: 'assets/img/carousel/8.webp', alt: 'Sainikpuri salon image 8' },
+	public imagesSainikpuri = [
+		{
+			path: 'assets/img/carousel/sainikpuri/1.jpg',
+			alt: 'Sainikpuri salon image 1',
+		},
+		{
+			path: 'assets/img/carousel/sainikpuri/2.jpg',
+			alt: 'Sainikpuri salon image 2',
+		},
+		{
+			path: 'assets/img/carousel/sainikpuri/3.jpg',
+			alt: 'Sainikpuri salon image 3',
+		},
+		{
+			path: 'assets/img/carousel/sainikpuri/4.jpg',
+			alt: 'Sainikpuri salon image 4',
+		},
+		{
+			path: 'assets/img/carousel/sainikpuri/5.jpg',
+			alt: 'Sainikpuri salon image 5',
+		},
+		{
+			path: 'assets/img/carousel/sainikpuri/6.jpg',
+			alt: 'Sainikpuri salon image 6',
+		},
+		{
+			path: 'assets/img/carousel/sainikpuri/7.jpg',
+			alt: 'Sainikpuri salon image 7',
+		},
+		{
+			path: 'assets/img/carousel/sainikpuri/8.jpg',
+			alt: 'Sainikpuri salon image 8',
+		},
+		{
+			path: 'assets/img/carousel/sainikpuri/9.jpg',
+			alt: 'Sainikpuri salon image 9',
+		},
 	];
 
 	public imagesMalkajgiri = [
-		{ path: 'assets/img/carousel/malkajgiri/1.webp', alt: 'Malkajgiri salon image 1' },
-		{ path: 'assets/img/carousel/malkajgiri/2.webp', alt: 'Malkajgiri salon image 2' },
-		{ path: 'assets/img/carousel/malkajgiri/3.webp', alt: 'Malkajgiri salon image 3' },
-		{ path: 'assets/img/carousel/malkajgiri/4.webp', alt: 'Malkajgiri salon image 4' },
-		{ path: 'assets/img/carousel/malkajgiri/5.webp', alt: 'Malkajgiri salon image 5' },
-		{ path: 'assets/img/carousel/malkajgiri/6.webp', alt: 'Malkajgiri salon image 6' },
-		{ path: 'assets/img/carousel/malkajgiri/7.webp', alt: 'Malkajgiri salon image 7' },
-		{ path: 'assets/img/carousel/malkajgiri/8.webp', alt: 'Malkajgiri salon image 8' },
-		{ path: 'assets/img/carousel/malkajgiri/9.webp', alt: 'Malkajgiri salon image 9' },
-		{ path: 'assets/img/carousel/malkajgiri/10.webp', alt: 'Malkajgiri salon image 10' },
-	]
+		{
+			path: 'assets/img/carousel/malkajgiri/1.webp',
+			alt: 'Malkajgiri salon image 1',
+		},
+		{
+			path: 'assets/img/carousel/malkajgiri/2.webp',
+			alt: 'Malkajgiri salon image 2',
+		},
+		{
+			path: 'assets/img/carousel/malkajgiri/3.webp',
+			alt: 'Malkajgiri salon image 3',
+		},
+		{
+			path: 'assets/img/carousel/malkajgiri/4.webp',
+			alt: 'Malkajgiri salon image 4',
+		},
+		{
+			path: 'assets/img/carousel/malkajgiri/5.webp',
+			alt: 'Malkajgiri salon image 5',
+		},
+		{
+			path: 'assets/img/carousel/malkajgiri/6.webp',
+			alt: 'Malkajgiri salon image 6',
+		},
+		{
+			path: 'assets/img/carousel/malkajgiri/7.webp',
+			alt: 'Malkajgiri salon image 7',
+		},
+		{
+			path: 'assets/img/carousel/malkajgiri/8.webp',
+			alt: 'Malkajgiri salon image 8',
+		},
+		{
+			path: 'assets/img/carousel/malkajgiri/9.webp',
+			alt: 'Malkajgiri salon image 9',
+		},
+		{
+			path: 'assets/img/carousel/malkajgiri/10.webp',
+			alt: 'Malkajgiri salon image 10',
+		},
+	];
 }
